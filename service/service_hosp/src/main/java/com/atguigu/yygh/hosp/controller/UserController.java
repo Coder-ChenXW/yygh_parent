b06a9d5af2baa1a0ed4db8cb73c158187cd3bf4e
ChenXW <chenxw1024@163.com>
2023-03-07 23:58:03 +0800

    完成与前端管理的相关交互的功能

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/UserController.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/UserController.java
@@ -1,17 +1,28 @@
 package com.atguigu.yygh.hosp.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.atguigu.yygh.common.result.R;
+import com.atguigu.yygh.model.acl.User;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/user/abc/xxx")
+@RequestMapping(value = "/admin/user")
+@CrossOrigin
 public class UserController {
 
-    @GetMapping("/info")
-    public String hello(){
-        return "hello world";
+    @PostMapping(value = "/login")
+    public R login(@RequestBody User user) {
+
+        return R.ok().data("token", "admin-token");
+
+    }
+
+    @GetMapping(value = "/info")
+    public R info(String token) {
+        return R.ok().data("token","[admin]")
+                .data("introduction","I am a super administrator")
+                .data("avatar","https://img1.baidu.com/it/u=693355708,2712228411&fm=253&fmt=auto&app=138&f=JPEG?w=640&h=418")
+                .data("name","Super Admin");
     }
 
 }
